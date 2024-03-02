b3775d249272b0e7db55d7ee5ae3f99d526da746
Code Keper <aliswork1122@gmail.com>
2024-03-02 15:11:56 +0500

    Fixed some issues and added comments.

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import pyttsx3
 
 if __name__ == '__main__':
+    print("Welcome to my first python project")
     while True:
-        print("Welcome to my first python project")
         myInput = input("Write what you want to say: ")
 
-        engine = pyttsx3.init() #initializing the text-to-speech engine
-        engine.setProperty('rate', 150) # Set the Speed of speech
+        # initializing the text-to-speech library:
+        speak = pyttsx3.init()
+        # speed of speech:
+        speak.setProperty('rate', 150)
 
-        if myInput == "exit":
-            engine.say("That's all for today. Thank you!")
+        # exit condition:
+        if myInput.lower() in ["exit"]:
+            speak.say("That's all for today. Thank you!")
+            speak.runAndWait()
             break
 
+        # speaking function:
         message = f"{myInput}"
-        engine.say(message)
+        speak.say(message)
 
-        engine.runAndWait()
+        # ensures the program not exit before completing the speaking function:
+        speak.runAndWait()
